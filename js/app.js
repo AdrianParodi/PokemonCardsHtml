fb90255edd93cfc7ebd08c47fb767c810814e1d3
Adrieto <parodiadrian3@gmail.com>
2024-08-02 11:07:40 -0300

    Formato de plantilla terminado. Botones de cambio de pagina agregados. Falta funcionalizarlos.

--- js/app.js
+++ js/app.js
@@ -1,61 +1,108 @@
-let containerCard = document.querySelector('#container-card') 
+// let containerCard = document.querySelector('#container-card') 
+// let contenedorTarjetas = document.querySelector('#contenedor-tarjetas')
+// // const pokemon = "pikachu"
 
-const pokemon = "pikachu"
-
-
-
-const obtenerPersonaje = (pokemon)=>{
-    const baseUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-    
-
-    // fetch(baseUrl)
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         const name = data.name;
-    //         const image = data.sprites.other.home.front_default ;
-    //         const height = data.height * 10; // meters
-    //         const weight = data.weight * 0.1; // kilograms
-    //         const abilities = data.abilities;
-    //         console.log(name, height, weight, abilities, image)
-    //     })
-    // }
-
-    fetch(baseUrl)
-    .then(response => response.json())
-    .then(data => {createCard(data)
-        // console.log("Hola", data.sprites.other.home.front_default)
-       
-    })
-}
-
-
-const createCard=(personaje)=>{
-    const{name,height, weight, abilities, sprites:{other:{home:{front_default: image}}}}=personaje  // utilicé destructuring
-    // console.log(name, height, weight, abilities, image)
-    containerCard.innerHTML = `<div class="card" style="width: 18rem;">
-            <img src=${image} class="card-img-top" alt="imagen de ${name}">
-            <div class="card-body">
-            <h4 class="card-title">${name.charAt(0).toUpperCase() + name.slice(1)}</h4>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-            </div>
-        </div>`
-}
-
-
-obtenerPersonaje(6)
-
-
-
-
-
+// // Funcion que realiza el request de informacion de un pokemon a la API
 // const obtenerPersonaje = (pokemon)=>{
-//     const baseUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+//     // pokemon puede ser el nombre o un numero entero 
+//     const endpoint = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+//     fetch(endpoint)
+//     .then(response => response.json())
+//     .then(data => {createCard(data)
+//         // console.log(data.name)
+//     })
+// }
+
+
+// const createCard = (personaje) => {
+//     const{name, height, weight, abilities, 
+//         sprites:{other:{home:{front_default: image}}}} = personaje  // utilicé destructuring
+
+//     // Creo el HTML para las habilidades
+//     const abilitiesHTML = abilities.map(ability => ability.ability.name).join(', ')
+
+//     containerCard.innerHTML = `<div class="card" style="width: 18rem;">
+//             <img src=${image} class="card-img-top" style="width: 18rem; height: 18rem" alt="imagen de ${name}">
+//             <div class="card-body">
+//             <h3 class="card-title">${name.charAt(0).toUpperCase() + name.slice(1)}</h4>
+//             <p class="card-text">
+//                 <b>Height:</b> ${(height * 0.1).toFixed(2) } m <br> 
+//                 <b>Weight:</b> ${(weight * 0.1).toFixed(2)} kg <br>
+//                 <b>Abilities:</b> ${abilitiesHTML}          
+//             </p>
+//             </div>
+//         </div>`
+//     return containerCard
+// }
+
+// // Voy a crear y mostrar una lista de pokemon en pantalla
+// let listaPokemon = [1, 4, 5, 'pikachu', '112'];
+
+// function crearListaTarjetas(listaPokemon){
+//     for (pokemon of listaPokemon){
+//         let tarjeta = obtenerPersonaje(pokemon)
+//         console.log(tarjeta)
+//         contenedorTarjetas.innerHTML += tarjeta
+//     }
+// }
+
+// // obtenerPersonaje('persian')
+// crearListaTarjetas(listaPokemon)
+
+
+
+
+// // const obtenerPersonaje = (pokemon)=>{
+// //     const baseUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
     
 
-//     fetch(baseUrl)
-//         .then(response => response.json())
-//         .then(data => console.log(data))
-//     }
+// //     fetch(baseUrl)
+// //         .then(response => response.json())
+// //         .then(data => console.log(data))
+// //     }
 
-// obtenerPersonaje(6)
+// // obtenerPersonaje(6)
+
+
+let containerCard = document.querySelector('#container-card');
+let contenedorTarjetas = document.querySelector('#contenedor-tarjetas');
+
+const obtenerPersonaje = async (pokemon) => {
+    const endpoint = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    return createCard(data);
+}
+
+const createCard = (personaje) => {
+    const {id, name, height, weight, abilities, 
+        sprites: { other: { home: { front_default: image } } } } = personaje;
+
+    const abilitiesHTML = abilities.map(ability => ability.ability.name).join(', ');
+
+    const cardHTML = `
+        <div class="card" style="width: 18rem; margin: 1.5rem">
+            <img src=${image} class="card-img-top" style="width: 18rem; height: 18rem" alt="imagen de ${name}">
+            <div class="card-body">
+                <h3 class="card-title">${id} - ${name.charAt(0).toUpperCase() + name.slice(1)}</h3>
+                <p class="card-text">
+                    <b>Height:</b> ${(height * 0.1).toFixed(2)} m <br> 
+                    <b>Weight:</b> ${(weight * 0.1).toFixed(2)} kg <br>
+                    <b>Abilities:</b> ${abilitiesHTML}
+                </p>
+            </div>
+        </div>`;
+    return cardHTML;
+}
+
+const listaPokemon = [4, 5, 6, 'pikachu', 26, 112, 125,126 ];
+
+const crearListaTarjetas = async (listaPokemon) => {
+    for (const pokemon of listaPokemon) {
+        const tarjeta = await obtenerPersonaje(pokemon);
+        console.log(tarjeta);
+        contenedorTarjetas.innerHTML += tarjeta;
+    }
+}
+
+crearListaTarjetas(listaPokemon);
